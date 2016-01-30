4be160193812294b306f5a77cdeec0748ca084d2
Giovanni Piumatti <giovanni.piumatti@polito.it>
2016-01-30 17:23:01 +0100

    Implement brazier logic
    Temporary placeholder code (uses color fading instead of animation)

---
+++ Assets/Scripts/BrazierLogic.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class BrazierLogic : MonoBehaviour
+{
+    [Tooltip("How many seconds before the brazier burns off")]
+    public float burnTime;
+
+    private float _lightingTime;
+    private bool _isLit;
+
+    private SpriteRenderer _sprite;
+
+    void Awake()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        GameManager.Instance.RegisterBrazier();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        _lightingTime = Time.fixedTime;
+        _isLit = true;
+        _sprite.color = Color.red;
+        GameManager.Instance.BrazierChanged(true);
+    }
+
+    void FixedUpdate()
+    {
+        var duration = Time.fixedTime - _lightingTime;
+        if (_isLit)
+        {
+            if (duration < burnTime)
+            {
+                _sprite.color = Color.Lerp(Color.red, Color.green, duration / burnTime);
+            }
+            else
+            {
+                _isLit = false;
+                _sprite.color = Color.green;
+                GameManager.Instance.BrazierChanged(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : UnitySingleton<GameManager>
+{
+    private int _totalBraziers, _litBraziers;
+
+    public override void Awake()
+    {
+        base.Awake();
+        _totalBraziers = 0;
+        _litBraziers = 0;
+    }
+
+    public void RegisterBrazier()
+    {
+        ++_totalBraziers;
+    }
+
+    public void BrazierChanged(bool lit)
+    {
+        if (lit) ++_litBraziers;
+        else --_litBraziers;
+
+        if (_litBraziers == _totalBraziers)
+            End();
+    }
+
+    void End()
+    {
+        Debug.Log("you win");
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        _totalBraziers = 0;
+        _litBraziers = 0;
+    }
+}
