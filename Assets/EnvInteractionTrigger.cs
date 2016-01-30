fcdafdf7057102bfbe07f45d87bbc3e361dc7d80
Alessandro Bertola <alessandro_bertola@hotmail.it>
2016-01-30 19:15:44 +0100

    Aggiunto ambiente

---
+++ Assets/EnvInteractionTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+public class EnvInteractionTrigger : MonoBehaviour {
+	public UnityEvent Entrata;
+	public UnityEvent Uscita;
+	// Use this for initialization
+
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+	
+		Entrata.Invoke ();
+	
+	}
+
+	void OnTriggerExit (Collider other){
+
+		Uscita.Invoke();
+	}
+}
