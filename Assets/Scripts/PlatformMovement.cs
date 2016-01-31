7d7b64b57b55314f15d44a699588b9d838cc3c15
Giovanni Piumatti <giovanni.piumatti@polito.it>
2016-01-31 11:56:15 +0100

    Implement sticking to moving platforms

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -11,6 +11,7 @@ public class PlatformMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        transform.tag = "MovingPlatform";
         startPosition = transform.position.x-currPosition;
 	}
 	
---
+++ Assets/Scripts/StickToMovingPlatform.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickToMovingPlatform : MonoBehaviour
+{
+    private Transform _initialParent;
+
+    void Start()
+    {
+        _initialParent = transform.parent;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "MovingPlatform")
+        {
+            transform.parent = collision.collider.transform;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "MovingPlatform")
+        {
+            transform.parent = _initialParent;
+        }
+    }
+}
